3d2290f7ed186fea178234a8652f6e48d41cba7a
Russell Matney <russell.matney@gmail.com>
2020-01-19 02:23:16 -0500

    feat: establish sub, db, events for tetris nmspc

--- src/toying/tetris.cljs
+++ src/toying/tetris.cljs
@@ -1,6 +1,8 @@
-(ns toying.tetris)
+(ns toying.tetris
+ (:require
+  [re-frame.core :as rf]))
 
-(def game-state
+(def initial-game-state
   {:grid
    [[0 0 0 0 0]
     [0 0 0 0 0]
@@ -13,20 +15,55 @@
     [0 0 0 0 0]
     [0 0 0 0 0]]})
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DB
+
+(def initial-db
+  {:game-state initial-game-state})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Events
+
+(rf/reg-event-db
+ ::init-db
+ (fn [db]
+   (assoc db ::tetris initial-db)))
+
+;; this is not quite right, but meh for now
+(rf/dispatch-sync [::init-db])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Subs
+
+(rf/reg-sub
+ ::tetris-db
+ (fn [db]
+   (::tetris db)))
+
+(rf/reg-sub
+ ::game-state
+ :<- [::tetris-db]
+ (fn [db]
+   (:game-state db)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Views
+
 (defn stage []
-  [:div
-   (for [row (:grid game-state)]
-     [:div
-      {:style
-       {:display "flex"}}
-      (for [cell row]
-        [:div
-         {:style
-          {:max-width "30px"
-           :max-height "30px"
-           :width "30px"
-           :height "30px"
-           :background "powderblue"
-           :border "black solid 1px"}}
-         ""])])])
+  (let [game-state @(rf/subscribe [::game-state])]
+   [:div
+    (for [row (:grid game-state)]
+      [:div
+       {:style
+        {:display "flex"}}
+       (for [cell row]
+         [:div
+          {:style
+           {:max-width "30px"
+            :max-height "30px"
+            :width "30px"
+            :height "30px"
+            :background "powderblue"
+            :border "black solid 1px"}}
+          ""])])]))
 
