a7bd83933ba5c956fbe511f301af8d73c7aca746
Russell Matney <russell.matney@gmail.com>
2020-02-01 23:12:02 -0500

    feat: write about page

--- src/games/tetris/views/about.cljs
+++ src/games/tetris/views/about.cljs
@@ -4,12 +4,40 @@
    [games.tetris.events :as tetris.events]
    [games.tetris.views.components :refer [widget display-label]]))
 
+(defn about
+  []
+  [:div
+   {:style
+    {:padding "5.5rem"}}
+   [:h3
+    {:style
+     {:margin-bottom "2rem"}}
+    "About"]
+   [:p
+    "This game was created for itch.io's "
+    [:a {:href "https://itch.io/jam/finally-finish-something-2020"}
+     "Finally Finish Something 2020"]
+    " Game Jam."]
+   [:p
+    "The code is open source and available on github at "
+    [:a {:href "https://github.com/russmatney/reframe-games"}
+     " russmatney/reframe-games"]
+    "."]
+   [:p "The engine was built using ClojureScript, Reagent, and Re-Frame."]
+   [:p
+    "The NES-style graphics come from "
+    [:a {:href "https://github.com/nostalgic-css/NES.css"} "NES.css"]
+    "."]
+   [:p "Thanks for playing!"]
+   [:p "Press g to return to the game."]])
 
 (defn view []
   [widget
    {:style
     {:width "100%"}
-    :label "about"}])
+    :children
+    [^{:key "child"}
+     [about]]}])
 
 (comment
   (rf/dispatch [::tetris.events/set-view :about])
